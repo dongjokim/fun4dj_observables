1291ec67762ad448662efe16e426075456fb4515
DongJo Kim <dong.jo.kim@cern.ch>
2022-11-15 10:11:20 +0200

    add stat errors

--- Fig1_AllObs_fun4dj.py
+++ Fig1_AllObs_fun4dj.py
@@ -64,7 +64,7 @@ gr = f_SPC.Get("{:s}{:s}".format(obsTypeStr_SPC[0],"_Stat"));
 x,y,_,yerr = JPyPlotRatio.TGraphErrorsToNumpy(gr);
 df['Centrality'] = x.tolist()
 
-df_cols = ['ObsType','Observables', 'Centrality' , 'Correlation']
+df_cols = ['ObsType','Observables', 'Centrality' , 'Correlation',"stat_err"]
 df_new = pd.DataFrame(columns=df_cols)
 
 #SPC
@@ -72,7 +72,7 @@ for i in range(0,len(obsTypeStr_SPC)):
 	gr = f_SPC.Get("{:s}{:s}".format(obsTypeStr_SPC[i],"_Stat"));
 	x,y,_,yerr = JPyPlotRatio.TGraphErrorsToNumpy(gr);
 	for j in range(0,len(x)):
-		df_new = df_new.append({'ObsType': "$\\langle\\cos\\left(a_{1} n_1 \\Psi_{n_1}+\\cdots+ a_{k} n_k \\Psi_{n_k}\\right) \\rangle_{GE}$",'Observables': plabel_SPC[i], 'Centrality': x[j], 'Correlation': y[j]}, ignore_index=True) # yuck
+		df_new = df_new.append({'ObsType': "$\\langle\\cos\\left(a_{1} n_1 \\Psi_{n_1}+\\cdots+ a_{k} n_k \\Psi_{n_k}\\right) \\rangle_{GE}$",'Observables': plabel_SPC[i], 'Centrality': x[j], 'Correlation': y[j],"stat_err":yerr[j]}, ignore_index=True) # yuck
 	df[obsTypeStr_SPC[i]] = y.tolist()
 	#g = sns.scatterplot(data=df, x="Centrality", y=obsTypeStr_SPC[i], size=obsTypeStr_SPC[i], legend=False, sizes=(20, 400))
 
@@ -81,7 +81,7 @@ for i in range(0,len(obsTypeStr_HSC)):
 	gr = f_HSC.Get("graph_{:s}ALICE".format(obsTypeStr_HSC[i]));
 	x,y,_,yerr = JPyPlotRatio.TGraphErrorsToNumpy(gr);
 	for j in range(0,len(x)):
-		df_new = df_new.append({'ObsType': "SC$(k,l,m)=\\left<v_k^2v_l^2v_m^2\\right>_c$",'Observables': plabel_HSC[i], 'Centrality': x[j], 'Correlation': y[j]}, ignore_index=True) # yuck
+		df_new = df_new.append({'ObsType': "SC$(k,l,m)=\\left<v_k^2v_l^2v_m^2\\right>_c$",'Observables': plabel_HSC[i], 'Centrality': x[j], 'Correlation': y[j],"stat_err":yerr[j]}, ignore_index=True) # yuck
 #	df[obsTypeStr_HSC[i]] = y.tolist()
 #	sns.scatterplot(data=df, x="Centrality", y=obsTypeStr_HSC[i], size=obsTypeStr_HSC[i], legend=False, sizes=(20, 400))
 
@@ -96,7 +96,7 @@ for index,s in enumerate(list(scpubd)):
 		#arrays = RemovePoints(scpubd[s],np.array([6,7]));
 		print(scpubd[s][0],scpubd[s][1])
 		for j in range(0,len(scpubd[s][0])):
-			df_new = df_new.append({'ObsType': "SC$(n,m)=\\left<v_n^2v_m^2\\right>_c$",'Observables': label, 'Centrality': scpubd[s][0][j], 'Correlation': scpubd[s][1][j]}, ignore_index=True) # yuck
+			df_new = df_new.append({'ObsType': "SC$(n,m)=\\left<v_n^2v_m^2\\right>_c$",'Observables': label, 'Centrality': scpubd[s][0][j], 'Correlation': scpubd[s][1][j],"stat_err":scpubd[s][2][j]}, ignore_index=True) # yuck
 			
 # colors = {"SPC":'red', "HSC":'blue'}
 # markers = {"SPC": 's', "HSC": 'o'}
@@ -121,6 +121,8 @@ g.set(xlim=(-1,60),ylim=(-0.25,1.1))
 
 # show the graph
 plt.text(0.85,0.75,toptitle,fontsize=10);
+
+plt.savefig("figs/allobs_fun4dj.pdf")
 plt.show()
 
 
